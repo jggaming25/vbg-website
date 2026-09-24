a69eb5f65144456fb88bc3eaf6a8c6571afd0252
Jann Genzmann <genzmannjann@gmail.com>
2026-09-24 18:50:02 +0200

    API-URL fuer GitHub Pages gesetzt

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 // API-Basis-URL.
 // GitHub-Pages (statisch) hier die Render-URL eintragen, z.B.:
 // window.VBG_API_BASE = "https://vbg-website.onrender.com";
-window.VBG_API_BASE = "";
+window.VBG_API_BASE = "https://vbg-website.onrender.com";
